0e83f8f6dbd2303f199ab3429de3a0637cc81b3b
João Paulo Barbosa <joaopbrb@gmail.com>
2020-11-23 19:47:40 -0300

    fixed equal sign breaking if you pressed it twice in a row

--- scripts/controller/calccontroller.js
+++ scripts/controller/calccontroller.js
@@ -6,7 +6,8 @@ class CalcController {
         this._displayCalcEl =  document.querySelector("#display")
         this._dateEl = document.querySelector("#data")
         this._timeEl = document.querySelector("#hora")
-        
+        this._lastOperator = ''
+        this._lastNumber = ''
         this._currentDate
         this.initialize()
         this.initButtonEvents()
@@ -49,12 +50,25 @@ class CalcController {
             console.log(this._operation)
         }
     }
+
+    getResult(){
+        return eval(this._operation.join(""))
+    }
     calc(){
         let last = ''
         if (this._operation.length > 3) {
-            let last = this._operation.pop()  
+            last = this._operation.pop()
+
+            this._lastNumber = this.getResult() 
+
         }
-        let result = eval(this._operation.join(""))
+
+        if (this._operation.length == 3) {
+            this._lastNumber = this.getLastItem(false)
+        }
+        console.log('_lastOperator', this._lastOperator)
+        console.log('_lastNumber', this._lastNumber)
+        let result = this.getResult() 
         if (last == '%') {
             result /= 100
             this._operation = [result]
@@ -63,15 +77,20 @@ class CalcController {
             if (last) this._operation.push(last)
         }
         this.setLastNumberToDisplay()
-    }
-    setLastNumberToDisplay(){
-        let lastNumber
+        }
+
+    getLastItem(isOperator = true) {
+        let lastItem
         for (let i = this._operation.length-1; i >= 0; i--){
-            if (!this.isOperator(this._operation[i])){
-                lastNumber = this._operation[i]
+            if (this.isOperator(this._operation[i]) == isOperator){
+                lastItem = this._operation[i]
                 break
             }
         }
+        return lastItem
+    }
+    setLastNumberToDisplay(){
+        let lastNumber = this.getLastItem(false)
         if (!lastNumber) lastNumber = 0
         this.displayCalc = lastNumber
     }
